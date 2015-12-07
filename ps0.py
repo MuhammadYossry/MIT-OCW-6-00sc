ec4311952600abd35396e0bea3b2ba51056536a0
Muhammad Yossry <mohamedyosry3000@gmail.com>
2015-12-07 19:19:33 +0200

    Assigment 0

---
+++ ps0.py
@@ -0,0 +1,8 @@
+# A simple program to ask user for their birthdate and lastname then prints them
+print('Enter your date of birth:')
+birth_date = raw_input('**')
+
+print('Enter your last name:')
+last_name = raw_input('**')
+
+print last_name, birth_date
